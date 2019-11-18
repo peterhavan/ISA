dcac6361a540bbba0652f4c72382ecbb012631da
Peter Havan <xhavan00@stud.fit.vutbr.cz>
2019-11-18 13:19:13 +0100

    backup before refractoring

--- isa-tazatel.c
+++ isa-tazatel.c
@@ -45,15 +45,11 @@ int main(int argc, char* argv[])
 	bool wflag = false, qflag = false, dflag = false, ipv6Flag = false, ipv4Flag = false;;
 	char destinationAddress[100], whoIsAddress[100], errbuf[PCAP_ERRBUF_SIZE];
 	char entryAddress[100], dnsServer[100];
-	//char sourceIp4[32], sourceIp6[50];
-	//char *dev;
 	while ((opt = getopt(argc, argv, "q:d:w:")) !=  -1)
 	{
 		switch (opt)
 		{
 			case 'q':
-				//entryAddress = (char *) malloc(sizeof(optarg)+2*sizeof(char));
-				//printf("%ld", sizeof(optarg)+2*sizeof(char));
 				strcpy(entryAddress, optarg);
 				qflag = true;
 				break;
@@ -76,83 +72,37 @@ int main(int argc, char* argv[])
 	strcpy(hbuf, entryAddress);
 	if (isValidIpv4Address(entryAddress))
 	{
-		//printf("IpV4\n");
 		struct sockaddr_in sa;
 		sa.sin_family = AF_INET;
 		inet_pton(AF_INET, entryAddress, &sa.sin_addr.s_addr);
-		//sa.sin_addr.s_addr = inet_addr("81.2.195.254");
-
-		if (getnameinfo((struct sockaddr*)&sa, sizeof(sa), hbuf, sizeof(hbuf), sbuf, sizeof(sbuf), NI_NUMERICHOST));
+		if (getnameinfo((struct sockaddr*)&sa, sizeof(sa), hbuf, sizeof(hbuf), sbuf, sizeof(sbuf), 0));
 			printf("host=%s, serv=%s\n", hbuf, sbuf);
 	}
 	else if(isValidIpv6Address(entryAddress))
 	{
-		//printf("IpV6\n");
 		struct sockaddr_in6 sa6;
 		sa6.sin6_family = AF_INET6;
 		inet_pton(AF_INET6, entryAddress, &sa6.sin6_addr.s6_addr);
-		if (getnameinfo((struct sockaddr*)&sa6, sizeof(sa6), hbuf, sizeof(hbuf), sbuf, sizeof(sbuf), NI_NUMERICHOST | NI_NUMERICSERV) == 0)
+		if (getnameinfo((struct sockaddr*)&sa6, sizeof(sa6), hbuf, sizeof(hbuf), sbuf, sizeof(sbuf), 0))
 			printf("host=%s, serv=%s\n", hbuf, sbuf);
 	}
 
-	//printf("entryAddress: %s\n", entryAddress);
+	char aRecords[MAXRECORDS][ARECORDLEN];
+	char aaaaRecords[MAXRECORDS][AAAARECORDLEN];
 	res_init();
 	if (dflag)
 		inet_pton(AF_INET, dnsServer, &(_res.nsaddr_list[0].sin_addr.s_addr));
 	printf("========DNS========\n");
-	resolveDns(hbuf, ns_t_a);
-	resolveDns(hbuf, ns_t_aaaa);
-	resolveDns(hbuf, ns_t_mx);
-	resolveDns(hbuf, ns_t_cname);
-	resolveDns(hbuf, ns_t_ns);
-	resolveDns(hbuf, ns_t_soa);
-	resolveDns(hbuf, ns_t_ptr);
+	resolveDns(hbuf, ns_t_a, aRecords, aaaaRecords);
+	resolveDns(hbuf, ns_t_aaaa, aRecords, aaaaRecords);
+	resolveDns(hbuf, ns_t_mx, aRecords, aaaaRecords);
+	resolveDns(hbuf, ns_t_cname, aRecords, aaaaRecords);
+	resolveDns(hbuf, ns_t_ns, aRecords, aaaaRecords);
+	resolveDns(hbuf, ns_t_soa, aRecords, aaaaRecords);
+	resolveDns(entryAddress, ns_t_ptr, aRecords, aaaaRecords);
 
-		/*struct sockaddr_in sa1; // could be IPv4 if you want
-        char host[1024];
-
-        sa1.sin_family = AF_INET;
-        sa1.sin_addr.s_addr = inet_addr("81.2.195.254");
-
-        getnameinfo((struct sockaddr*)&sa1, sizeof sa1, host, sizeof host, NULL, 0, 0);
-        printf("hostname: %s", host);*/
-		//return 0;
-
-
-	//printf("%d\n", isValidIpv4Address(entryAddress));
-
-	//entryAddress[strlen(entryAddress)] = '\r';
-	//entryAddress[strlen(entryAddress)] = '\n';
-
-/*	char hbuf[1024], sbuf[NI_MAXSERV];
-	if (isValidIpv4Address(entryAddress))
-	{
-		printf("IpV4\n");
-		struct sockaddr_in sa;
-		sa.sin_family = AF_INET;
-		inet_pton(AF_INET, entryAddress, &sa.sin_addr.s_addr);
-		//sa.sin_addr.s_addr = inet_addr("81.2.195.254");
-
-		if (getnameinfo((struct sockaddr*)&sa, sizeof(sa), hbuf, sizeof(hbuf), sbuf, sizeof(sbuf), 0));
-  		printf("host=%s, serv=%s\n", hbuf, sbuf);
-	}
-	else if(isValidIpv6Address(entryAddress))
-	{
-		printf("IpV6\n");
-		struct sockaddr_in6 sa6;
-		sa6.sin6_family = AF_INET6;
-		inet_pton(AF_INET6, entryAddress, &sa6.sin6_addr.s6_addr);
-		if (getnameinfo((struct sockaddr*)&sa6, sizeof(sa6), hbuf, sizeof(hbuf), sbuf, sizeof(sbuf), NI_NUMERICHOST | NI_NUMERICSERV) == 0)
-			printf("host=%s, serv=%s\n", hbuf, sbuf);
-	}*/
-	//strcpy(entryAddress, hbuf);
-	//else
-	//{
-		entryAddress[strlen(entryAddress)] = '\r';
-		entryAddress[strlen(entryAddress)] = '\n';
-	//}
-
-	//return 0;
+	entryAddress[strlen(entryAddress)] = '\r';
+	entryAddress[strlen(entryAddress)] = '\n';
 
 	/* getting whois adress */
 	/* inspired by https://gist.github.com/jirihnidek/bf7a2363e480491da72301b228b35d5d by jirihnidek */
@@ -193,24 +143,19 @@ int main(int argc, char* argv[])
 	}
 
 	int sock;
-  struct sockaddr_in servaddr, cli;
-  // socket create and varification
+  struct sockaddr_in servaddr;
   sock = socket(AF_INET, SOCK_STREAM, 0);
   if (sock == -1)
 		errorMsg("socket(): FAILED");
   bzero(&servaddr, sizeof(servaddr));
-  // assign IP, PORT
   servaddr.sin_family = AF_INET;
   servaddr.sin_addr.s_addr = inet_addr(destinationAddress);
   servaddr.sin_port = htons(PORT);
-  // connect the client socket to server socket
   if (connect(sock, (SA*)&servaddr, sizeof(servaddr)) != 0)
 		errorMsg("connect(): FAILED");
 	char buffer[65535];
-	//char line[2048];
 	char *line = NULL;
-	//char *addr = "vutbr.cz\r\n";
-	//send(sock, addr, strlen(addr), 0);
+
 	send(sock, entryAddress, strlen(entryAddress), 0);
 	int n = read(sock, buffer, 65535);
 
@@ -227,32 +172,48 @@ int main(int argc, char* argv[])
 		//printf ("%s\n", line);
 		line = strtok(NULL, "\n");
 	}
-	//printf("%d\t", n);
-	//n = read(sock, buffer, 65535);
-
-	//printf("%s\n", buffer);
-
 
 	//./isa-tazatel -q google.com -w whois.markmonitor.com
-  // close the socket
   close(sock);
-	//free(entryAddress);
-	char tmp[2048];
-
-	inet_ntop(AF_INET, &_res.nsaddr_list[0].sin_addr.s_addr, tmp, sizeof(tmp));
-	printf("%s\n", tmp);
+	printf("--------------------------------\n");
+	for (int o = 0; o < 10; o++)
+	{
+		if(strlen(aRecords[o]) > 0)
+			printf("A:\t%s\n", aRecords[o]);
+		if(strlen(aaaaRecords[o]) > 0)
+			printf("AAAA:\t%s\n", aaaaRecords[o]);
+	}
 	return 0;
 }
 
-int resolveDns(char *entryAddress, ns_type nsType)
+int resolveDns(char *entryAddress, ns_type nsType,
+	 						char aRecords[MAXRECORDS][ARECORDLEN],
+							char aaaaRecords[MAXRECORDS][AAAARECORDLEN])
 {
 	u_char answer[1024] = "";
-	//res_init();
-	int rv = res_query(entryAddress, ns_c_in, nsType, answer, sizeof(answer));
-	//printf("rv=%d\n", rv);
-	if (rv <= 0)
-		return -1;
-
+	int aCount = 0;
+	int aaaaCount = 0;
+	int rv;
+	if ((nsType == ns_t_ptr) && (isValidIpv4Address(entryAddress)))
+	{
+		uint32_t addrInt;
+		inet_pton(AF_INET, entryAddress, &addrInt);
+		addrInt = htonl(addrInt);
+		char tmp[100];
+		inet_ntop(AF_INET, &addrInt, tmp, sizeof(tmp));
+		strcpy(&tmp[strlen(tmp)], ".in-addr.arpa");
+		//printf("%ld\n", strlen(tmp));
+		//printf("========tmp%s======\n", tmp);
+		rv = res_query(tmp, ns_c_in, nsType, answer, sizeof(answer));
+		if (rv <= 0)
+			return -1;
+	}
+	else
+	{
+		rv = res_query(entryAddress, ns_c_in, nsType, answer, sizeof(answer));
+		if (rv <= 0)
+			return -1;
+	}
 	ns_msg handle;
 	if (ns_initparse(answer, rv, &handle) < 0)
 		errorMsg("ERROR:ns_initparse()");
@@ -262,8 +223,6 @@ int resolveDns(char *entryAddress, ns_type nsType)
 	for (int i = 0; i < counter; i++)
 	{
 		ns_parserr(&handle, ns_s_an, i, &rr);
-		/*if (ns_rr_type(rr) == ns_t_a)
-			printf("A record found\n");*/
 		switch (ns_rr_type(rr))
 		{
 			case ns_t_soa:
@@ -276,11 +235,17 @@ int resolveDns(char *entryAddress, ns_type nsType)
 				printf("A:\t");
 				inet_ntop(AF_INET, ns_rr_rdata(rr), buf, sizeof(buf));
 				printf("%s\n", buf);
+				strncpy(aRecords[aCount], buf, sizeof(aRecords[aCount]));
+				aCount++;
 				break;
 			case ns_t_aaaa:
 				printf("AAAA:\t");
 				inet_ntop(AF_INET6, ns_rr_rdata(rr), buf, sizeof(buf));
 				printf("%s\n", buf);
+				strncpy(aaaaRecords[aaaaCount], buf, AAAARECORDLEN);
+				//strcpy(aaaaRecords[aaaaCount], buf);
+				printf("%s\n", aaaaRecords[aaaaCount]);
+				aaaaCount++;
 				break;
 			case ns_t_mx:
 				printf("MX:\t");
