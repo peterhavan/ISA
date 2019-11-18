bb6fd2da34f32475cbf496b11a077c664d9a705d
Peter Havan <xhavan00@stud.fit.vutbr.cz>
2019-11-18 13:19:26 +0100

    backup before refractoring

--- isa-tazatel.h
+++ isa-tazatel.h
@@ -1,6 +1,11 @@
+#define MAXRECORDS 10
+#define ARECORDLEN 32
+#define AAAARECORDLEN 50
 void errorMsg(char *msg);
 bool isValidIpv4Address(char *ipAddress);
 bool isValidIpv6Address(char *ipAddress);
-int resolveDns(char * entryAddress, ns_type ns_t);
+int resolveDns(char * entryAddress, ns_type ns_t,
+              char[MAXRECORDS][ARECORDLEN],
+              char[MAXRECORDS][AAAARECORDLEN]);
 
 extern const char *_res_opcodes[];
